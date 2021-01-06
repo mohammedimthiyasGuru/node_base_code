e7180ca991529c1e3b06252fde36851bbd720c67
Hari <worksintriton@gmail.com>
2021-01-06 16:52:46 +0530

    first base code

---
+++ app.js
@@ -0,0 +1,86 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');	
+var fileUpload = require('express-fileupload');
+var pdf = require('html-pdf');
+const puppeteer = require('puppeteer');
+const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
+var fs = require('fs');
+var pug = require ('pug');
+
+/*Routing*/
+var responseMiddleware = require('./middlewares/response.middleware');
+var ActivityRouter = require('./routes/Activity.routes');
+
+
+/*Database connectivity*/
+
+var BaseUrl = "http://54.214.141.11:3000"; 
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost:27017/Salveo'); 
+var db = mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+
+var app = express();
+
+app.use(fileUpload());
+app.use(responseMiddleware());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+
+/*Response settings*/
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+  next();
+});
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/api/', express.static(path.join(__dirname, 'public')));
+app.use('/api/', express.static(path.join(__dirname, 'routes')));
+
+
+app.use ('/api/activity', ActivityRouter);
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
